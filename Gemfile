e9ed47a97970c762167dfd78ce72bb5683511449
Pradeep Sethi <psethi@gmail.com>
2015-04-02 12:48:34 +0530

    added gems, added more methods to main

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 gem 'json'
 gem 'sinatra'
+gem 'httparty'
+gem 'awesome_print'
+
+group :test do
+  gem "rspec"
+  gem "rack-test"
+end
+
--- main.rb
+++ main.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
+require File.expand_path("../environment", __FILE__)
 
 
 get "/" do
 	erb :index
 end
+
+get "/check_in" do
+	erb :check_in	
+end
+
+
